6e95926aa3705abf3b4791cba5426622f32ced5a
unknown <CaroleM@MURI-PC-CM.bscg.local>
2020-02-13 10:43:36 +0100

    knit

--- rscript/import_and_prepare.R
+++ rscript/import_and_prepare.R
@@ -1,13 +1,12 @@
+#import data
+
 AB_NYC <- read.csv("data/AB_NYC_2019.csv", header=TRUE)
 str(AB_NYC)
 summary(AB_NYC)
 
 #add distance to time square to model
-#Times Square, Manhattan, NY, USA
-#Latitude and longitude coordinates are: 40.758896, -73.985130.
-
+#Times Square, Manhattan, NY, USA, Latitude and longitude coordinates are: 40.758896, -73.985130.
 #https://cran.r-project.org/web/packages/geosphere/geosphere.pdf
-
 install.packages("geosphere")
 library(geosphere)
 
@@ -20,3 +19,7 @@ dist.timessquare <- distGeo(p1=coord, p2=c(-73.985130, 40.758896))
 AB_NYC <- cbind(AB_NYC,dist.timessquare)
 head(AB_NYC)
 str(AB_NYC)
+
+#remove all high prices 
+
+AB_NYC_lowprice <- AB_NYC[AB_NYC$price <= 500,]
